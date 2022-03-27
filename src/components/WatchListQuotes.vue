94d17f2ed3bc0bcbeb2e7f20401a331065620f3b
jroberto-inv <jroberto@inv.com.vc>
2022-03-27 15:18:17 -0300

    create component WatchListQuotes

---
+++ src/components/WatchListQuotes.vue
@@ -0,0 +1,18 @@
+<template>
+  <ListQuotes :quotes="quotes" />
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import ListQuotes from './ListQuotes'
+
+export default {
+  name: 'WatchListQuotes',
+  components: { ListQuotes },
+  setup() {
+    const quotes = ref({})
+
+    return { quotes }
+  }
+}
+</script>

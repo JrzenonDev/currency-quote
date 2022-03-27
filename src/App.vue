06e2ed5f61f7c3ef0250942bdcd1b4964f362cdb
jroberto-inv <jroberto@inv.com.vc>
2022-03-27 14:47:53 -0300

    use component ListQuotes in App

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="container grid-lg">
+  <div class="container grid-lg my-2 py-2">
+    <div class="card">
+      <div class="card-header">
+        <div class="h4">Todas as moedas</div>
+      </div>
+      <div class="card-body">
+        <ListQuotes :quotes="quotes" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { onMounted, reactive, toRefs } from 'vue'
 import api from '@/services/api'
+import ListQuotes from './components/ListQuotes'
 
 export default {
   name: 'App',
+  components: { ListQuotes },
   setup() {
 
     const data = reactive({
